9c4c89041210aead03c2ad6e2070d80d68cc90f1
Hal <hgrforrest@gmail.com>
2021-09-07 15:11:41 -0400

    more spinning cubes

--- js/linesThree.js
+++ js/linesThree.js
@@ -23,6 +23,8 @@ const points = [];
 points.push(new THREE.Vector3(- 10, 0, 0));
 points.push(new THREE.Vector3(0, 10, 0));
 points.push(new THREE.Vector3(10, 0, 0));
+points.push(new THREE.Vector3(0, -10, 0));
+points.push(new THREE.Vector3(-10, 0, 0));
 
 const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
@@ -30,3 +32,35 @@ const line = new THREE.Line(geometry, material);
 
 scene.add(line);
 renderer.render(scene, camera);
+
+const geometry2 = new THREE.BoxGeometry();
+const material2 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry2, material2);
+scene.add(cube);
+const geometry3 = new THREE.BoxGeometry();
+const material3 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const cube3 = new THREE.Mesh(geometry3, material3);
+cube3.position.set(0, -10, -10);
+scene.add(cube3);
+const cube4 = new THREE.Mesh(geometry3, material3);
+cube4.position.set(0, 10, -10);
+scene.add(cube4);
+
+camera.position.z = 10;
+function animate() {
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.02;
+	cube3.rotation.x += 0.01;
+	cube3.rotation.z += 0.04;
+	cube4.rotation.z += 0.06;
+	camera.position.z += 0.01;
+}
+animate();
+
+
+
+
+
+
